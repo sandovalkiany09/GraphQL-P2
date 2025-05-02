1f4758384279291d3be69083bf7c4c7037bad605
Kiany <sandovalkiany@gmail.com>
2025-05-01 23:49:54 -0600

    Agrego listado de Videos

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,5 @@
 const Perfil = require('./models/perfilModel');
+const Video = require('./models/videoModel');
 
 const resolvers = {
   Query: {
@@ -8,6 +9,13 @@ const resolvers = {
         return perfiles;
       } catch (error) {
         throw new Error('Error al obtener los perfiles');
+      }
+    },
+    videos: async (_, { playlistId }) => {
+      try {
+        return await Video.find({ playlistId });
+      } catch (error) {
+        throw new Error('Error al obtener los videos');
       }
     },
   },
--- typeDefs.js
+++ typeDefs.js
@@ -2,6 +2,21 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
+  type Video {
+    id: ID!
+    nombre: String!
+    url: String!
+    descripcion: String
+    playlistId: ID!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type Query {
+    videos(playlistId: ID!): [Video!]!
+    perfiles(usuarioId: ID!): [Perfil!]!
+  }
+
   type Perfil {
   id: ID!
   nombre: String!
