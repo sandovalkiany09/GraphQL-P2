c370b6d5a6e317235393661b3e04f27cbfb66b27
Kiany <sandovalkiany@gmail.com>
2025-05-03 08:46:55 -0600

    Agrego listado playlist y búsquedad de videos

--- resolvers.js
+++ resolvers.js
@@ -1,8 +1,43 @@
+const Playlist = require('./models/playlistModel');
 const Perfil = require('./models/perfilModel');
 const Video = require('./models/videoModel');
 
 const resolvers = {
   Query: {
+    playlistsPorPerfil: async (_, { perfilId }) => {
+      const playlists = await Playlist.find({ perfilesAsociados: perfilId });
+      return Promise.all(playlists.map(async (p) => {
+        const cantidadVideos = await Video.countDocuments({ playlistId: p._id });
+        return {
+          id: p._id.toString(),
+          nombre: p.nombre,
+          cantidadVideos
+        };
+      }));
+    },
+    playlists: async (_, { usuarioId }) => {
+      try {
+        const playlists = await Playlist.find({ usuarioId });
+
+        // Para cada playlist, contar sus videos
+        const conConteo = await Promise.all(
+          playlists.map(async (pl) => {
+            const cantidad = await Video.countDocuments({ playlistId: pl._id });
+            return {
+              id: pl._id.toString(),
+              nombre: pl.nombre,
+              usuarioId: pl.usuarioId.toString(),
+              perfilesAsociados: pl.perfilesAsociados.map(p => p.toString()),
+              cantidadVideos: cantidad
+            };
+          })
+        );
+
+        return conConteo;
+      } catch (error) {
+        throw new Error("Error al obtener las playlists");
+      }
+    },
     perfiles: async (_, { usuarioId }) => {
       try {
         const perfiles = await Perfil.find({ usuarioId });
@@ -11,6 +46,9 @@ const resolvers = {
         throw new Error('Error al obtener los perfiles');
       }
     },
+    videosPorPlaylist: async (_, { playlistId }) => {
+      return await Video.find({ playlistId });
+    },
     videos: async (_, { playlistId }) => {
       try {
         return await Video.find({ playlistId });
@@ -18,6 +56,40 @@ const resolvers = {
         throw new Error('Error al obtener los videos');
       }
     },
+    async buscarVideosPorPerfil(_, { perfilId, query }) {
+      try {
+        // 1. Buscar playlists donde el perfil esté asociado
+        const playlists = await Playlist.find({
+          perfilesAsociados: perfilId
+        });
+    
+        const playlistIds = playlists.map(p => p._id);
+    
+        // 2. Buscar videos que estén en esas playlists y coincidan con el nombre
+        const videos = await Video.find({
+          playlistId: { $in: playlistIds },
+          nombre: { $regex: query, $options: 'i' }
+        });
+    
+        // 3. Mapear y agregar nombre de la lista a cada video
+        return videos.map(video => {
+          const playlist = playlists.find(p => p._id.toString() === video.playlistId.toString());
+          return {
+            id: video._id.toString(), // Asegúrate de incluir el id explícitamente
+            nombre: video.nombre,
+            url: video.url,
+            descripcion: video.descripcion,
+            playlistId: video.playlistId,
+            createdAt: video.createdAt,
+            updatedAt: video.updatedAt,
+            listaNombre: playlist ? playlist.nombre : 'Desconocida'
+          };
+        });
+      } catch (error) {
+        console.error('Error al buscar videos por perfil:', error);
+        throw new Error('No se pudo buscar videos');
+      }
+    },    
   },
 };
 
--- typeDefs.js
+++ typeDefs.js
@@ -2,19 +2,45 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
+  type Playlist {
+    id: ID!
+    nombre: String!
+    cantidadVideos: Int!
+  }
+  type Query {
+    playlistsPorPerfil(perfilId: ID!): [Playlist!]!
+  }
+
+  type Playlist {
+    id: ID!
+    nombre: String!
+    usuarioId: ID!
+    perfilesAsociados: [ID!]!
+    cantidadVideos: Int!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type Query {
+    playlists(usuarioId: ID!): [Playlist!]!
+  }
+
   type Video {
     id: ID!
     nombre: String!
     url: String!
     descripcion: String
     playlistId: ID!
+    listaNombre: String
     createdAt: String!
     updatedAt: String!
   }
 
   type Query {
+    videosPorPlaylist(playlistId: ID!): [Video!]!
     videos(playlistId: ID!): [Video!]!
     perfiles(usuarioId: ID!): [Perfil!]!
+    buscarVideosPorPerfil(perfilId: ID!, query: String!): [Video]
   }
 
   type Perfil {
